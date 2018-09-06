9d7b1f6103400d6d721093b4f29b8dc4d4be7de7
aryansharma23124 <aryansharma23124@gmail.com>
2018-09-06 11:48:34 +0530

    Added path variables to compile and run C++ scripts

--- python_main_home.py
+++ python_main_home.py
@@ -4,6 +4,10 @@ a=int(input("Enter"))
 if(a==1):
     with open("output.txt", "w+") as output:
         subprocess.call(["python", "./hello.py"], stdout=output);
+elif(a==2):
+    os.system("g++ test_cp.cpp -o test_cp ")
+    with open("output.txt","w+") as output:
+        subprocess.call("./test_cp",stdout=output)
 else:
     os.system("gcc test.c -o test")
     #from subprocess import call
---
+++ test_cp.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+cout<<"I am in c++ now";
+return 0;
+}
