cf2680a81fef363b47c6dd6a40aac1ec8c2fc574
aryansharma23124 <aryansharma23124@gmail.com>
2018-09-06 11:31:04 +0530

    Added system paths for c file

--- python_main_home.py
+++ python_main_home.py
@@ -1,9 +1,11 @@
+import os
 import subprocess
 a=int(input("Enter"))
 if(a==1):
     with open("output.txt", "w+") as output:
         subprocess.call(["python", "./hello.py"], stdout=output);
 else:
+    os.system("gcc test.c -o test")
     #from subprocess import call
     with open("output.txt","w+") as output:
         subprocess.call("./test",stdout=output);
